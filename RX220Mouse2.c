15e407ec73c130bf982d1243a9a5c027e20e9110
SunSunSun-23413011 <23413011tk@stu.yamato-u.ac.jp>
2026-01-10 14:37:18 +0900

    微調整

--- RX220Mouse2.c
+++ RX220Mouse2.c
@@ -686,8 +686,8 @@ void load_param( void )
     slalom_step_out_table[gspeed_index_for_value(300)] = 40; // 速度300用
     slalom_step_forward_table[gspeed_index_for_value(400)] = 80;  // 速度400用
     slalom_step_out_table[gspeed_index_for_value(400)] = 34; // 速度400用
-    slalom_step_forward_table[gspeed_index_for_value(500)] = 40;  // 速度500用
-    slalom_step_out_table[gspeed_index_for_value(500)] = 32; // 速度500用
+    slalom_step_forward_table[gspeed_index_for_value(500)] = 8;  // 速度500用
+    slalom_step_out_table[gspeed_index_for_value(500)] = 30; // 速度500用
     slalom_step_forward_table[gspeed_index_for_value(600)] = 2;  // 速度600用
     slalom_step_out_table[gspeed_index_for_value(600)] = 18; // 速度600用
     slalom_inner_speed_table[gspeed_index_for_value(300)] = 100; // 速度300用
@@ -1701,7 +1701,7 @@ void slalom_search( int goal_x, int goal_y, int spd, int mode )
     }
     if(motion == 1 || motion ==3 ){
       // 前の行動が右折or左折の場合、距離を少なくする。
-      while( STEP < GO_STEP / 2 - 250 );  // 少し進む
+      while( STEP < GO_STEP / 2 - 270 );  // 少し進む
     }else if( (next_motion == 1 || next_motion == 3) && GSPEEDvar >= 600 ){ // 速度700以上で次の行動が右折or左折の場合、前進しない
       control_mode = 0; // 姿勢制御OFF
       while( STEP < 200 );  // ほとんど進まない
